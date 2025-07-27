ec57006c997eca7295b184569f4786a2ffb2d086
anirudha23 <114842691+anirudha23@users.noreply.github.com>
2025-07-27 23:32:32 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TWELVE_API_KEY = os.getenv("TWELVE_API_KEY")
-DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
-SYMBOL = "BTC/USD"
-TIMEFRAME = "15min"
