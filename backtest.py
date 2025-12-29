e84f9cb3162858e4bc6647117b87ce645cee7608
anirudha23 <114842691+anirudha23@users.noreply.github.com>
2025-12-29 20:25:05 +0530

    Delete backtest.py

--- backtest.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_json("trade_history.json")
-
-print("Trades:", len(df))
-print("Win rate:", (df["result"] == "TP").mean())
-print("Total PnL:", df["pnl"].sum())
-print("Average PnL:", df["pnl"].mean())
